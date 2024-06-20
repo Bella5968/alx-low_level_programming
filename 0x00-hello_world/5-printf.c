41c957a5917f7f3521a0c129eec4b0937a039b24
Bella5968 <abitimenge@gmail.com>
2024-06-20 04:54:26 -0700

    this is printf command task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is printf command
+ *
+ * Return: 0 after complation
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
