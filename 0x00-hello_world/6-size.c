759838bfe6eb410b980a2affad4c2514fbb44522
Bella5968 <abitimenge@gmail.com>
2024-06-20 12:26:03 -0700

    size variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-/**
- * main - my sister name
- *
- * Return: 0 on completion
- */
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof(float));
-	return (0);
+	    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+		    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+		        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+			    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+			        return (0);
 }
+
