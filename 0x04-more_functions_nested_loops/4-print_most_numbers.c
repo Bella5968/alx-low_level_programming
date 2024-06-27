f33425631cafbbcda31d082203734375528ea02e
Bella5968 <abitimenge@gmail.com>
2024-06-27 13:16:29 -0700

    dont print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,17 +8,16 @@
 
 void print_most_numbers(void)
 {
-	int a;
-
-	for (a = 48; c < 58; a++)
-	{
-		if (a != 50)
-		{
-			if (a != 52)
-			{
-				-putchar(a);
-			}
-		}
-	}
-	_putchar('\n');
+int c;
+for (c = 48; c < 58; c++)
+{
+if (c != 50)
+{
+if (c != 52)
+{
+_putchar(c);
+}	
+}
+}
+_putchar('\n');
 }
