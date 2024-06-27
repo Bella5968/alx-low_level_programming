7adca0c88c57d079f246c825701226a12e91d6c6
Bella5968 <abitimenge@gmail.com>
2024-06-27 12:54:58 -0700

    print numb

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all except 2 and 4
+ *
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; c < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+				-putchar(a);
+			}
+		}
+	}
+	_putchar('\n');
+}
