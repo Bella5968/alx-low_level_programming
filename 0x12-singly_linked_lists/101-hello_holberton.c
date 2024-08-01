451588555fe5572127713e29d1159c094cb10d8b
Bella5968 <abitimenge@gmail.com>
2024-08-01 05:37:51 -0700

    bonus new

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "Hello, Holberton" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char message[] = "Hello, Holberton!";
+
+printf("%s\n", message);
+
+return (0);
+}
+
