69983d708aa6c0f7a92dedbc384a0b85bdcf305c
Bella5968 <abitimenge@gmail.com>
2024-10-10 10:24:20 -0700

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,36 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the hash table
+ * hash_table_create - Creates a hash table
+ * @size: Size of the array
  *
- * Return: pointer to hash table
+ * Return: Pointer to the newly created hash table,
+ * or NULL if something went wrong.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+hash_table_t *new_table;
+unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
+/* Allocate memory for the hash table structure */
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
-	table = calloc(1, sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
+/* Allocate memory for the array of pointers */
+new_table->array = malloc(sizeof(hash_node_t *) * size);
+if (new_table->array == NULL)
+{
+free(new_table);
+return (NULL);
+}
 
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
-	{
-		free(table);
-		return (NULL);
-	}
-	return (table);
+/* Initialize each pointer in the array to NULL */
+for (i = 0; i < size; i++)
+new_table->array[i] = NULL;
+
+/* Set the size of the hash table */
+new_table->size = size;
+
+return (new_table);
 }
